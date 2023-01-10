0bfadecb9f6784fc1cb12ad392c92a9aea443e92
vaishnavipanwar <88323011+vaishnavipanwar@users.noreply.github.com>
2023-01-10 22:59:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-function preload(){
+function preload() {
 
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    camera = createCapture(VIDEO);
+    camera.size(300, 300);
+    camera.hide();
+    posenet = ml5.poseNet(camera, modelLoaded);
+    posenet.on('pose', gotResult);
 }
 
-function draw(){
-
+function draw() {
+    image(camera, 0, 0, 300, 300);
 }
 
-function images(){
-    save("filter.jpg"); 
- }
- 
- 
+function images() {
+    save("filter.jpg");
+}
+
+function modelLoaded() {
+    console.log("posenet is iniciated");
+}
+
+function gotResult(results) {
+    if (results.length > 0) {
+        console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x =" + nose_x);
+        console.log("nose y =" + nose_y);
+    }
+}
